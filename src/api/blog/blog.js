24787493a0b235cfa3cca30170755343d8a8accf
Double327 <180303665@qq.com>
2021-06-01 22:57:30 +0800

    博客API

--- src/api/blog/blog.js
+++ src/api/blog/blog.js
@@ -1,1 +1,39 @@
-import '@/'
+import request from '@/axios';
+
+export function addBlog(blog) {
+    return request({
+        url: '/blog/',
+        method: 'post',
+        data: blog
+    });
+}
+
+export function deleteBlog(ids) {
+    return request({
+        url: '/blog/blog/' + ids,
+        method: 'delete',
+    });
+}
+
+export function editBlog(blog) {
+    return request({
+        url: '/blog/blog/',
+        method: 'put',
+        data: blog
+    });
+}
+
+export function blogDetails(id) {
+    return request({
+        url: '/blog/blog/' + id,
+        method: 'get'
+    });
+}
+
+export function blogList(blog) {
+    return request({
+        url: '/blog/blog/list',
+        method: 'get',
+        params: blog
+    });
+}
